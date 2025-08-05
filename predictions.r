6a30e05fcb774443f0d3b0c052e0f84feb6f5539
praj <jangleprajakta2295@gmail.com>
2025-08-05 22:04:22 +1000

    Added predictions.r that uses LBAM model for valence calculation

---
+++ predictions.r
@@ -0,0 +1,13 @@
+T1 <- Sys.time()
+word_embeddings <- textEmbed(data,
+    model = "mixedbread-ai/mxbai-embed-large-v1",
+    batch_size = 50
+)
+T2 <- Sys.time()
+
+
+valence <- textPredict(
+    model_info = "valence_facebook_mxbai23_eijsbroek2024",
+    word_embeddings = word_embeddings$texts,
+    save_dir = "valence/"
+)
