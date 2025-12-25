81af8c575f49937bafb332fa2ad585ac7c2c6375
praj <jangleprajakta2295@gmail.com>
2025-12-25 18:59:30 +1100

    Add comments

--- data_descriptives.r
+++ data_descriptives.r
@@ -1,5 +1,5 @@
-imm_desc <- textDescriptives(imm_data$pos)
-md_desc <- textDescriptives(shuffled_mddata$pos)
+imm_desc <- textDescriptives(imm_data$pos) # 3.9k posts
+md_desc <- textDescriptives(shuffled_mddata$pos) # 28k posts
 
 print(imm_desc, width = Inf)
 print(md_desc, width = Inf)
